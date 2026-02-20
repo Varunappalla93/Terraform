347d851d7ba9ef4cf82cfd1abeb2d73756fdedad
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-20 14:28:07 +0530

    cndtns

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -4,7 +4,7 @@
 
 resource "aws_instance" "example" {
   ami                    = var.ami_id
-  instance_type          = var.environment=="dev" ? "t3.micro":"t3.small"
+  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small"
   vpc_security_group_ids = [aws_security_group.allow-ttls.id]
   tags = var.ec2-tags
 }
