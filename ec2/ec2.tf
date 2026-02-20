4835a804097c5445e4a19a736d646024f4a87d17
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-20 08:06:56 +0530

    ec2tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "allow-ttls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-       ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
