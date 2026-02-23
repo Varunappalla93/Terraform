056afe74e0d6c5fbc8aa82b5cd36e46b9e070398
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-23 16:46:20 +0530

    functions

--- loops/route53.tf
+++ loops/route53.tf
@@ -9,3 +9,13 @@ resource "aws_route53_record" "www" {
   ttl     = 1
   records = [aws_instance.loops[count.index].private_ip]
 }
+
+# roboshop.vardevops.online -> maps to public ip address using index function
+resource "aws_route53_record" "www" {
+  count   = length(var.instances)
+  zone_id = var.zone_id
+  name    = "roboshop.${var.domain_name}"      # mongodb.vardevops.online and . here is called as interpolation
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.loops[index(var.instances,"frontend")].public_ip]
+}
