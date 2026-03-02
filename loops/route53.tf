73d80738638687c4e320defffff11e40ad96cbb1
Varunappalla1993 <an.varun1993@gmail.com>
2026-03-02 13:09:02 +0530

    r53

--- loops/route53.tf
+++ loops/route53.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "www" {
 resource "aws_route53_record" "www" {
   count   = length(var.instances)
   zone_id = var.zone_id
-  name    = "roboshop.${var.domain_name}"      # mongodb.vardevops.online and . here is called as interpolation
+  name    = "roboshop.${var.domain_name}"      # roboshop.vardevops.online and . here is called as interpolation
   type    = "A"
   ttl     = 1
   records = [aws_instance.loops[index(var.instances,"frontend")].public_ip]
